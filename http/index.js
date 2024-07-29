01b161474820a72e3a70cc1cf5bc74194d195490
MKA <mixailrojnow@yandex.ru>
2024-07-29 23:20:26 +0300

    разобрался как отдавать контент в файлах, сделал api endpoint

--- http/index.js
+++ http/index.js
@@ -1,64 +1,51 @@
 import http from 'node:http'
 import fs from 'node:fs/promises'
 
+const API_KEY = 'dfshb08fs7dbysdufhg34098y-9e8wbywrugh43'
+
 const PORT = 3000
 
-const loadIndexHtml = async () => {
-	return await fs.readFile('./http/src/index.html', 'utf-8')
-}
-
 let server = http.createServer(async (req, res) => {
-	switch (req.method) {
-		case 'GET':
-			switch (req.url) {
-				case '/':
-					res.writeHead(200, {
-						'Content-Type': 'text/html; charset=utf-8'
-					})
-					res.end(await loadIndexHtml())
-					break
-			}
-			break
-		case 'POST':
-			switch (req.url) {
-				case '/':
-					let variables = []
-					res.writeHead(200, {
-						'Content-Type': 'text/html; charset=utf-8'
-					})
-					req.on('data', (data) => {
-						data = data.toString().split('&')
-						let variable = {}
-						let nameOfVar
-						for (let cred of data) {
-							if (cred.split('=')[0] === 'nameOfVar') {
-								nameOfVar = cred.split('=')[1]
-								variable[nameOfVar] = ''
-							} else if (cred.split('=')[0] === 'valueOfVar') {
-								variable[nameOfVar] = cred.split('=')[1]
-								variables.push(variable)
-								variable = {}
-							}
-						}
-						console.log(variables)
-					})
-					res.end(await loadIndexHtml())
-					break
-				case '/api':
+	switch (req.url) {
+		// case '/':
+		// 	res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' })
+		// 	res.end(await fs.readFile('./http/src/index.html', 'utf-8'))
+		// 	break
+		// case '/style.css':
+		// 	res.writeHead(200, { 'Content-Type': 'text/css' })
+		// 	res.end(await fs.readFile('./http/src/style.css', 'utf-8'))
+		// 	break
+		// case '/index.js':
+		// 	res.writeHead(200, { 'Content-Type': 'text/js' })
+		// 	res.end(await fs.readFile('./http/src/index.js', 'utf-8'))
+		// 	break
+		case '/api':
+			if (req.method === 'POST') {
+				if (req.headers['api-key'] === API_KEY) {
+					res.writeHead(200, { 'Content-Type': 'text/json' })
 					let body = ''
-					res.writeHead(200, {
-						'Content-Type': 'text/json; charset=utf-8'
-					})
 					req.on('data', (data) => {
 						body += data
 					})
 					req.on('end', () => {
-						console.log(body)
+						try {
+							let data = JSON.parse(body)
+							data = [data]
+							console.log(data)
+							res.end(JSON.stringify({ message: 'data received' }))
+						} catch (e) {
+							res.end(JSON.stringify({ message: 'incorrect data' }))
+						}
 					})
-					res.end()
 					break
+				} else {
+					res.writeHead(400, { 'Content-Type': 'text/json' })
+					res.end(JSON.stringify({ message: 'bad key' }))
+				}
+			} else {
+				res.writeHead(400, { 'Content-Type': 'text/json' })
+				res.end(JSON.stringify({ message: 'incorrect method' }))
 			}
-			break
 	}
 })
 
