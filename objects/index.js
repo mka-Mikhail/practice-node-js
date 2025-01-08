e64e6205677b9f39620f0c7339daef6958a9b93c
mka-Mikhail <mixailrojnow@yandex.ru>
2025-01-08 14:22:13 +0300

    метод toJSON() в объектах

--- objects/index.js
+++ objects/index.js
@@ -1,3 +1,40 @@
 import { ver } from './object.js'
 
-ver()
+// ver()
+
+function unwrap(data) {
+  return JSON.parse(JSON.stringify(data))
+}
+
+let myObj = {
+  field1: 'field1',
+  innerObj: {
+    qqq: 'qqq'
+  },
+  toJSON() {
+    return this.field1
+  }
+}
+
+let myArr = [
+  {
+    id: 1,
+    innerObj: {
+      qqq: 'qqq'
+    },
+    toJSON() {
+      return this.id
+    }
+  },
+  {
+    id: 2,
+    innerObj: {
+      ddd: 'ddd'
+    },
+    toJSON() {
+      return this.id
+    }
+  }
+]
+
+console.log(unwrap(myArr))
